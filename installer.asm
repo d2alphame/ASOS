6d9b44a6f39a96e18dd600c5ac0931c5ef4d5039
Deji Adegbite <d2alphame@yahoo.com>
2023-05-07 13:39:08 +0100

    Save boot device of installer

--- installer.asm
+++ installer.asm
@@ -2,15 +2,21 @@
 [Bits 16]
 [Org 0x7c00]
 
-jmp 0x00:MAIN                                   ; A trick to set cs = 0
+jmp 0x00:MAIN                                       ; A trick to set cs = 0
 
 MAIN:
-    cli                                         ; Clear interrupts. No interruptions before we're done setting up
+    cli                                             ; Clear interrupts. No interruptions before we're done setting up
+
+    ; Setup a flat segment with cs = ds = es = ss = 0
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0xFFFE
+    mov sp, 0xFFFE                                  ; Point the stack to the top of the segment
+
+    sti                                             ; We're done setting up. Set interrupts again
+
+    mov byte [.installer_boot_device], dl           ; Save the boot device. This will be handed to us by BIOS
 
     mov ah, 0x0E                                    ; Print in teletype mode
     mov al, 'A'                                     ; The character to print
@@ -18,7 +24,8 @@ MAIN:
     int 10h                                         ; Interrupt for printing to screen
     jmp $                                           ; Hang here
 
-;times 510 - ($ - $$) db 0                       ; Padd with 0s up to 510 bytes
-;dw 0xAA55                                       ; The boot signature
+    .installer_boot_device: db 0                    ; We'll save the boot device here as soon as we find it
 
-;times 1474560 - ($ - $$) db 0                   ; Pad with more 0s to make up 1.44MB floppy disk
+times 510 - ($ - $$) db 0                       ; Padd with 0s up to 510 bytes
+dw 0xAA55                                       ; The boot signature
+times 1474560 - ($ - $$) db 0                   ; Pad with more 0s to make up 1.44MB floppy disk
