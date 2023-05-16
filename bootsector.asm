c70ca373b3c943cd8166490abd53e326a7ffac1e
Deji Adegbite <d2alphame@yahoo.com>
2023-05-16 17:21:09 +0100

    Make minro adjustments to filesystem description

--- bootsector.asm
+++ bootsector.asm
@@ -1,5 +1,9 @@
 ; The boot sector of ASOS
 [Bits 16]
-[Org 0x7c00]
+[Org 0x7C00]
 
+jmp 0x00:MAIN                   ; Jump over asos filesystem data below and also set cs = 0
+align 8                         ; ASOS filesystem requires this
 
+TOTAL_SECTORS:  dq 0x400000     ; Total number of sectors on the disk
+        
