cb4146c8b07b8dc5792e89152419694c581e2de0
Deji Adegbite <d2alphame@yahoo.com>
2023-05-18 17:45:32 +0100

    Bootsector work in progress

--- bootsector.asm
+++ bootsector.asm
@@ -13,3 +13,18 @@ SIGNATURE:              dw 0xA51                    ; Signature of the tool, pro
 LAST_CLUSTER:           dw 0xFFFF                   ; Number of clusters that make up this filesystem - 1. This implies there are 0x10000 (65536) clusters
 LBA_FIRST:              dq 0x00                     ; LBA of first sector of this filesystem
 LBA_LAST:               dq 0x7FFFF                  ; LBA of last sector of the filesystem
+FREE_FOR_CUSTOM_USE:    dq 0x00                     ; Free for custom use
+RESERVED:               dq 0x00                     ; Reserved for future use
+FLAGS:                  dd 0x00                     ; Flags. Not in use for now
+XOR_CHECKSUM:           dd 0x00                     ; XOR checksum of the above struc (starting from the jump instruction down to the flags)
+
+MAIN:
+    cli                                             ; Clear interrupts. No interruptions before we're done setting up
+    
+    ; Setup a flat segment with cs = ds = es = ss = 0
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x600                                   ; Use 0x500 to 0x600 for the stack giving us 256 bytes of stack space
+    sti                                             ; We're done setting up. Set interrupts again
