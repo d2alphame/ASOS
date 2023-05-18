fc9f004b3eda72e5bdfde4290a8f426be568a32b
Deji Adegbite <d2alphame@yahoo.com>
2023-05-18 17:06:56 +0100

    Complete filesystem specifications

--- bootsector.asm
+++ bootsector.asm
@@ -5,6 +5,6 @@
 jmp 0x00:MAIN                   ; Jump over asos filesystem data below and also set cs = 0
 align 8                         ; ASOS filesystem requires this
 
-TOTAL_SECTORS:  dq 0x08         ; Total number of sectors on the disk
+
 
         
